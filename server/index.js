b86eb4455d00ceb3e95956d229a4c93a11b0570d
artkiller971 <koshevnkikod@gmail.com>
2022-08-04 12:56:01 +0300

    asd

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
-import dotenv from "dotenv";
-
 import addRoutes from './routes/index.js';
 
-dotenv.config();
 // eslint-disable-next-line no-unused-vars
 export default (app, options) => {
   addRoutes(app);
