c15def1faedddfa6795dc275468615a96472345a
Anton <koshevnikod@gmail.com>
2025-06-03 19:29:47 +0300

    Removed required field in the task model

--- server/models/Task.cjs
+++ server/models/Task.cjs
@@ -8,11 +8,11 @@ module.exports = class Task extends BaseModel {
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['name', 'description', 'statusId', 'creatorId'],
+      required: ['name', 'statusId', 'creatorId'],
       properties: {
         id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
-        description: { type: 'string', minLength: 1 },
+        description: { type: 'string' },
         statusId: { type: 'integer' },
         creatorId: { type: 'integer' },
         executorId: { type: 'number', nullable: true },
